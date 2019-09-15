876d05bc6eab8f5c2e07014d55d8c7e438551c97
mattvlaw <mattvlaw@gmail.com>
2019-09-15 16:57:07 -0400

    added a random_seed to spec. also reverted example so you have to run setup_example. i think it will be easier to maintain that way.

---
+++ experiment_types/intent_filter_experiment.py
@@ -0,0 +1,9 @@
+from experiment_types import Experiment
+
+class IntentFilterExperiment:
+    def run(self, specs):
+        specs.environment_params['logfile'] = specs.logpath
+        specs.agent_params['logfile'] = specs.logpath
+        specs.environment.set_params(specs.environment_params)
+        specs.runner.set_params(specs.agent_params)
+        specs.runner.run(specs.environment,specs.n_steps)
