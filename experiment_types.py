112e3f1d9a7da53b21f439a677e0ff76dd1101da
mattvlaw <mattvlaw@gmail.com>
2019-09-15 17:18:37 -0400

    removed experiment_types.py which was there by accident.

--- experiment_types.py
+++
@@ -1,5 +0,0 @@
-experiment_runners = {}
-# add any agents you write to this list and __all__
-import experiment_types.agent
-import experiment_types.intent_filter
-__all__ = ['agent','intent_filter']
