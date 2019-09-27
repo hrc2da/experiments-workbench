1daacebceba99dc4a9711fc216b256b0a47f72ca
mattvlaw <mattvlaw@gmail.com>
2019-09-27 00:01:57 -0400

    some cleanup of specs

--- experiment_types/agent_experiment.py
+++ experiment_types/agent_experiment.py
@@ -2,8 +2,10 @@ from experiment_types import Experiment
 
 class AgentExperiment:
     def run(self, specs):
-        specs.environment_params['logfile'] = specs.logpath
-        specs.agent_params['logfile'] = specs.logpath
-        specs.environment.set_params(specs.environment_params)
-        specs.agent.set_params(specs.agent_params)
-        return specs.agent.run(specs.environment,specs.n_steps)
+        specs['agent'] = specs['agent']()
+        specs['environment'] = specs['environment']()
+        specs['environment_params']['logfile'] = specs['logpath']
+        specs['agent_params']['logfile'] = specs['logpath']
+        specs['environment'].set_params(specs['environment_params'])
+        specs['agent'].set_params(specs['agent_params'])
+        return specs['agent'].run(specs['environment'],specs['n_steps'])
